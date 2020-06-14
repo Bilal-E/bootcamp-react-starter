5b0297057bf4f54ed1c2bb4e5d35498aa6a7c19b
bilalelahi <bilaalelahi@gmail.com>
2020-06-15 01:17:58 +0500

    remove-logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          
-          First Edit!! Git action used to deploy react-app using surge!
-        </p>
+     <header>
+     <p>
+       First Edit!! Git action used to deploy react-app using surge!
+     </p>
      
       </header>
     </div>
